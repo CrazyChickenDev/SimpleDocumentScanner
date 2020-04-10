6dcf4eca0067d6bad3b8c6bf28175a4544b2dc13
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-10 23:18:50 +0100

    python file

--- docScanner.py
+++ docScanner.py
@@ -46,3 +46,13 @@ image = imutils.resize(image, height = 500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+for c in cnts:
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	if len(approx) == 4:
+		screenCnt = approx
