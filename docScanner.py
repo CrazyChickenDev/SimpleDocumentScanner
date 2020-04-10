f5036b21df30372d658fe8b22bee75144babd03d
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-10 23:18:16 +0100

    python file

--- docScanner.py
+++ docScanner.py
@@ -43,3 +43,6 @@ ratio = image.shape[0] / 500.0
 orig = image.copy()
 image = imutils.resize(image, height = 500)
 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
